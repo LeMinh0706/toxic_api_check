4195fbff65fc97c4af706b5151476fd8bf32d5e9
LeMinh0706 <leminhken124356@gmail.com>
2024-12-26 00:50:18 +0700

    cors

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from typing import Union
 import uvicorn
 
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 import sys
@@ -23,6 +24,16 @@ phobert, cnn = load_models(
 )
         
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Sentence(BaseModel):
     content: str
